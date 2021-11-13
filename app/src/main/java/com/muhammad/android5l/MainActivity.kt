45dde3643b91e32704f2364e474070701311c1e9
Android5-211-L <ardiudin1213@gmail.com>
2021-11-13 14:27:06 +0800

    memanggil komponen di layout dan memanggil nilainya

--- app/src/main/java/com/muhammad/android5l/MainActivity.kt
+++ app/src/main/java/com/muhammad/android5l/MainActivity.kt
@@ -2,10 +2,32 @@ package com.muhammad.android5l
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val phoneEditText = findViewById<EditText>(R.id.phoneEditText)
+        val tanggallahirEditText = findViewById<EditText>(R.id.tanggallahirEditText)
+        val jumlahsksEditText = findViewById<EditText>(R.id.jumlahsksEditText)
+
+        simpanButton.setOnClickListener {
+            val nama = namaEditText.text.toString()
+            var jumlahsks = jumlahsksEditText.text.toString().toInt()
+            jumlahsks = 160 - jumlahsks
+            Toast.makeText(this, "Sisa SKS... $jumlahsks ", Toast.LENGTH_SHORT).show()
+        }
+        batalButton.setOnClickListener {
+            Toast.makeText(this, "Batal", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
